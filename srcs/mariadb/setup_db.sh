7a470bf91215aa448d995158de60ce470d76c6d7
LeBojo <contact@jordan-chap.com>
2024-07-17 17:10:25 +0200

    Now the DB works, but can't connect with WP

---
+++ srcs/mariadb/setup_db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+
+
+mkdir -p /var/lib/mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
+
+if [ ! -e "/var/lib/mysql/$DB_NAME" ]
+then
+    mariadb-install-db --datadir=/var/lib/mysql --user=mysql
+    mysqld --user=mysql --datadir=/var/lib/mysql &
+    sleep 4
+    mariadb -e "CREATE DATABASE $DB_NAME ;"
+    mariadb -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+    mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+    mariadb -e "FLUSH PRIVILEGES;"
+    mysqladmin shutdown
+
+else
+    echo "Database already present"
+fi
+
+exec "$@"

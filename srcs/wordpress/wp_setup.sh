5845642b6ebe60f07166f49c2f5ace2c630ac8c8
LeBojo <contact@jordan-chap.com>
2024-07-17 16:37:47 +0200

    closest to workin

--- srcs/wordpress/wp_setup.sh
+++ srcs/wordpress/wp_setup.sh
@@ -13,6 +13,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core download
 	wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST" --dbprefix="$DB_PREFIX"
 	wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email=$WP_ADMIN_EMAIL
+	echo "WordPress installed"
 else
 	echo "WordPress already installed"
 fi
